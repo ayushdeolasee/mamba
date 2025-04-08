6f0aa64eb4303113cde26fe7bed7b226eb03dfca
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-04-08 21:43:09 +0530

    creating a basic cli in rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+use clap::Parser;
 
-    println!("Pattern: {:?}, path: {:?}", pattern, path);   
+#[derive(Parser)]
+struct Cli {
+    command: String,
+    args: Vec<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    if cli.args.is_empty() {
+        println!("No arguments provided.");
+        return;
+    }
+    if cli.command.is_empty() {
+        println!("No command provided.");
+        return;
+    }
+    
+    if cli.args.len() == 1 {
+        println!("Using project_name for env");
+        return;
+    } else if cli.args.len() == 2 {
+        println!("Using custom name for env");
+    } 
+
+    println!("Command: {:?}, args: {:?}", cli.command, cli.args);   
 }
