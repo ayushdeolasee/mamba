070285344768d3ef4f28454b218e4022e1b3e14e
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-04-08 22:24:28 +0530

    creating README.md file when using mamba create

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,10 @@ fn main() {
         if cli.args.len() == 1 {
             let project_name = cli.args[0].clone();
             fs::create_dir(&project_name).unwrap();
+            let mut readme_file = fs::File::create(format!("{}/README.md", project_name)).unwrap();  
             let mut python_file = fs::File::create(format!("{}/main.py", project_name)).unwrap(); 
             let mut toml_file = fs::File::create(format!("{}/py.toml", project_name)).unwrap();
+            readme_file.write_all(format!("# {}\n", project_name).as_bytes()).unwrap();
             python_file.write_all(b"").unwrap();
             toml_file.write_all(b"").unwrap();
 
@@ -32,11 +34,13 @@ fn main() {
         } else if cli.args.len() == 2 {
             let project_name = cli.args[0].clone();
             fs::create_dir(&project_name).unwrap();
+            let mut readme_file = fs::File::create(format!("{}/README.md", project_name)).unwrap();   
             let mut python_file = fs::File::create(format!("{}/main.py", project_name)).unwrap(); 
             let mut toml_file = fs::File::create(format!("{}/py.toml", project_name)).unwrap();
             python_file.write_all(b"").unwrap();
             toml_file.write_all(b"").unwrap(); 
-            
+            readme_file.write_all(format!("# {}\n", project_name).as_bytes()).unwrap();
+
             println!("Using custom name for env");
             return;
         } 
