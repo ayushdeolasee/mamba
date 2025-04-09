719055ab635c3e1506939390e5c2b7958e1852e5
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-04-09 16:08:18 +0530

    adding a few commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use std::fs;
 use std::io::Write;
+use std::process::{Command, Stdio};
 
 #[derive(Parser)]
 struct Cli {
@@ -10,14 +11,6 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    if cli.args.is_empty() {
-        println!("No arguments provided.");
-        return;
-    }
-    if cli.command.is_empty() {
-        println!("No command provided.");
-        return;
-    }
     if cli.command == "create" {
         if cli.args.len() == 1 {
             let project_name = cli.args[0].clone();
@@ -28,6 +21,23 @@ fn main() {
             readme_file.write_all(format!("# {}\n", project_name).as_bytes()).unwrap();
             python_file.write_all(b"").unwrap();
             toml_file.write_all(b"").unwrap();
+
+            let conda_status = Command::new("conda")
+                .arg("create")
+                .arg("--name")
+                .arg(&project_name)
+                .arg("python=3.12")
+                .arg("--yes")
+                // .stdout(Stdio::null()) 
+                .status()
+                .expect("Failed to create conda environment");
+
+            let conda_activate = Command::new("conda")
+                .arg("activate")
+                .arg(&project_name)
+                // .stdout(Stdio::null()) 
+                .status()
+                .expect("Failed to activate conda environment");
 
             println!("Using project_name for env");
             return;
@@ -44,6 +54,16 @@ fn main() {
             println!("Using custom name for env");
             return;
         } 
+    } if cli.command == "delete" {
+
+    } if cli.command == "install" {
+        
+    } if cli.command == "template" {
+
+    } if cli.command == "uninstall" {
+
+    } else {
+        println!("Command {:?} not found.", cli.command);
     }
     println!("Command: {:?}, args: {:?}", cli.command, cli.args);   
 }
